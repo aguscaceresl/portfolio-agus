f5f1823cdf21b833b740412e1d0cc8bec7d812ce
aguscaceresl <agustincacereslucero@gmail.com>
2024-12-11 21:48:32 +0100

    Add images, react-router-dom and modify index

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
-import Header from './components/Header'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Cards from './components/Cards';
+import MasSobreMi from './components/MasSobreMi';  // Asegúrate de que este componente exista
+import MasProyectos from './components/MasProyectos';
+import NewsLetter from './components/NewsLetter';
+import Contacto from './components/Contacto';
 
-function App() {
-
+export default function App() {
   return (
-    <>
-      <Header/>
-    </>
-  )
+    <Router>
+      <Header /> 
+      <Routes>
+        <Route path="/" element={<Cards />} />
+        <Route path="/sobre-mi" element={<MasSobreMi />} />
+        <Route path="/proyectos" element={<MasProyectos />} />
+        <Route path="/newsletter" element={<NewsLetter />} />
+        <Route path="/contacto" element={<Contacto />} />
+      </Routes>
+    </Router>
+  );
 }
-
-export default App
